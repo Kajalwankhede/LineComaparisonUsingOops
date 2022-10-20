ccfa15c62b376a87485c9033f9cb94479381f9a8
Kajakwankhede <Kajalwankhede@example.com>
2022-10-20 22:09:33 +0530

    Added my Employee Wage Computation Program

--- src/com/employeewage/EmployeeComputation.java
+++ src/com/employeewage/EmployeeComputation.java
@@ -1,16 +1,13 @@
 package com.employeewage;
-
 public class EmployeeComputation {
-    //Declaring the constants
-    private final int isPartTime = 1;
-    private final int isFullTime = 2;
-    private final int wagePerHour = 20;
-    private final int numWorkingDays = 20;
-    private final int maxHours = 100;
+    private int numWorkingDays = 20;
+    private int maxHours = 100;
+    private int isPartTime = 1;
+    private int isFullTime = 2;
+    private int wagePerHour = 20;
 
-    // Default Constructor
-    public EmployeeComputation() {
-        System.out.println("Welcome to Employee Wage Computation Program..!");
+    EmployeeComputation() {
+        System.out.println("\nWelcome to Employee Wage Computation Program");
     }
 
     @Override
@@ -25,25 +22,22 @@ public class EmployeeComputation {
     }
 
     public static void main(String[] args) {
-
-        //Initializing the constructor by creating the object of the class.
         EmployeeComputation employee = new EmployeeComputation();
-        System.out.println("Constants Taken for the computation program are below :\n" + employee);
+        System.out.println("\n" + employee);
         System.out.println();
-        System.out.println("Calculating the salary : ............");
-        printData(calculateSalary(employee)); //Parsing value from calculateSalary Method.
+        display(calculateSalary(employee));
 
     }
 
-    private static void printData(int[] calculateSalary) {
+    private static void display(int[] calculateSalary) {
 
         int monthlySalary = calculateSalary[0];
         int totalWorkingDays = calculateSalary[1];
         int totalWorkingHours = calculateSalary[2];
         System.out.println();
-        System.out.println("Total working days of an Employee is : " + totalWorkingDays + "\n");
-        System.out.println("Total working hour of an Employee is : " + totalWorkingHours + "\n");
-        System.out.println("Salary of an Employee in a month is : $" + monthlySalary);
+        System.out.println("Total working days of an Employee : " + totalWorkingDays + "\n");
+        System.out.println("Total working hour of an Employee : " + totalWorkingHours + "\n");
+        System.out.println("Salary of an Employee in a months : $" + monthlySalary);
 
 
     }
@@ -54,8 +48,8 @@ public class EmployeeComputation {
         final int isPartTime = 1;
         final int isFullTime = 2;
         //Declaring the variables
-        int randomCheck;
-        int empHour;
+        int randomCheck = 0;
+        int empHour=8;
         int wagePerHour = 20;
         int salary = 0;
         int numWorkingDays = 20;
@@ -64,50 +58,51 @@ public class EmployeeComputation {
         int totalWorkingHours = 0;
         int totalWorkingDays = 0;
 
-            while (totalWorkingDays < employee.numWorkingDays && totalWorkingHours < employee.maxHours) {
-                totalWorkingDays++;
-                randomCheck = (int) Math.floor((Math.random() * 10) % 3);
-                switch (randomCheck) {
-                    default:
-                        System.out.println("Employee is absent");
-                        empHour = 0;
-                        salary = empHour * wagePerHour;
-                        break;
-                    case isPartTime:
-                        System.out.println("Employee is working part-time");
-                        int partTimeHour = 4;
-                        empHour = partTimeHour;
-                        salary = empHour * wagePerHour;
-                        break;
-                    case isFullTime:
-                        System.out.println("Employee is working full-time");
-                        int fullTimeHour = 8;
-                        empHour = fullTimeHour;
-                        salary = empHour * wagePerHour;
-                        break;
-                    if (randomCheck == 0) {
-                        System.out.println("Employee is absent");
-                        empHour = 0;
-                        salary = empHour * employee.wagePerHour;
-                    } else if (randomCheck == employee.isPartTime) {
+        while (totalWorkingDays < employee.numWorkingDays && totalWorkingHours < employee.maxHours) {
+            totalWorkingDays++;
+            randomCheck = (int) Math.floor((Math.random() * 10) % 3);
+            switch (randomCheck) {
+                case 1:
+                    empHour = 4;
+                    System.out.println("Employee is Present for Half Day");
+                    break;
+                case 2:
+                    empHour = 8;
+                    System.out.println("Employee is Present for Full Day");
+                    break;
+                default:
+                    empHour = 0;
+                    System.out.println("Employee is Absent");
 
-                        System.out.println("Employee is working part-time");
-                        partTimeHour = 4;
-                        empHour = partTimeHour;
-                        salary = empHour * employee.wagePerHour;
-                    } else {
-                        System.out.println("Employee is working full-time");
-                        fullTimeHour = 8;
-                        empHour = fullTimeHour;
-                        salary = empHour * employee.wagePerHour;
-                    }
-                    monthlySalary += salary;
-                    totalWorkingHours += empHour;
-                }
+            }
+            int Total = wagePerHour * empHour;
+            System.out.println("Employee Wage is: " + Total);
+        }
+        if (randomCheck == 0) {
+            System.out.println("Employee is absent");
+            empHour = 0;
+            salary = empHour * employee.wagePerHour;
+        } else if (randomCheck == employee.isPartTime) {
+
+            System.out.println("Employee is part-time");
+            int partTimeHour = 4;
+            empHour = partTimeHour;
+            salary = empHour * employee.wagePerHour;
+        } else {
+            System.out.println("Employee is full-time");
+            int fullTimeHour = 8;
+            empHour = fullTimeHour;
+            salary = empHour * employee.wagePerHour;
+        }
+        monthlySalary += salary;
+        totalWorkingHours += empHour;
+
+
+        int[] employeeData = {monthlySalary, totalWorkingDays, totalWorkingHours};
+        return employeeData;
 
 
             }
-        int[] employeeData = {monthlySalary, totalWorkingDays, totalWorkingHours};
-        return employeeData;
+
     }
-}
+
